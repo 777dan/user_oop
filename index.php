a9f363a00ccde4927537075815221cd4b48e66e1
777dan <danya.nikitin7@gmail.com>
2023-01-21 09:27:35 +0200

    fixed problems with verification

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 require "user.php";
-$user = new User();
-$user->setLogin("Peter");
-$user->setPassword("abcde");
-echo $user->getLogin()."\n";
-if ($user->isValid()) echo "Пароль и логин совпадают";
-if (!$user->isValid()) echo "Пароль или логин не совпадает";
+$user = new User("Peter", "abcde");
+$user1 = new User("Alex", "321");
+print $user->isValid("Peter", "abcde") ? "Всё верно" : "Пароль или логины не совпадают";
+echo "\n";
+print $user1->isValid("Alex", "3221") ? "Всё верно" : "Пароль или логины не совпадают";
+echo "\n";
+echo $user->getLogin() . "\n";
+echo $user1->getLogin() . "\n";
--- user.php
+++ user.php
@@ -1,17 +1,15 @@
 <?php
 class User {
     public $login, $password;
-    public function setLogin($login){
-        $this->login = $login;
-    }
-    public function setPassword($password){
+    public function __construct($login, $password)
+    {
         $this->password = $password;
-        
+        $this->login = $login;
     }
     public function getLogin(){
         return $this->login;
     }
-    public function isValid($login = "Peter", $password = "abcde"){
+    public function isValid($login, $password){
         if ($this->login === $login && $this->password === $password){
             return true;
         } else {
